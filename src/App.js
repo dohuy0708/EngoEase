02476e0de7ba9dcf8c01e57d90243225848dc0bd
dohuy0708 <dohuy22314@gmail.com>
2024-12-20 10:54:24 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "../src/app/styles/App.css";
+
+function App() {
+  return (
+    <div className=" flex text-center text-blue-500 justify-center items-center">
+      <h1 className="text-4xl font-bold items-center justify-center">
+        Hé lô anh Huy Hoàng, anh đến code em đi ^-^!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
